2b6c3690dee9d8223394edb2a0dd5890f7d3a6aa
Mohammed Taher <tahercmi@outlook.com>
2019-01-06 14:01:36 +0530

    Added browser router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, combineReducers } from 'redux';
+import {BrowserRouter} from 'react-router-dom';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import { Provider } from 'react-redux';
 
